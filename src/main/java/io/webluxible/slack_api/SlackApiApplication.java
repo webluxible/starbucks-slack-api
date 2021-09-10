36f398c8f5b62fe12159ff7a4a1c87def22500e1
Jaime <lcalmsky@gmail.com>
2021-09-11 00:43:47 +0900

    Set up project initial configurations

---
+++ src/main/java/io/webluxible/slack_api/SlackApiApplication.java
@@ -0,0 +1,13 @@
+package io.webluxible.slack_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlackApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/webluxible/slack_api/SlackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.webluxible.slack_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlackApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
